e4f76ae8ae2629ba289cf3555996c8677b17233f
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2022-11-21 13:30:54 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (5408, 0, 304303.303843);
+
+--UPDATE sandbox SET int64_value = 21548 WHERE id = 30;
+
+--DELETE FROM sandbox WHERE id = 28;
